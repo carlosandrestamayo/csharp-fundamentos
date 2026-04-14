1a5edc850e95debd3888c46e5f86e3c4aaa760b0
carlosandrestamayo <carlosandrestamayo1285@gmail.com>
2026-04-14 18:40:36 -0500

    Update Prestamo.cs

--- src/biblioteca/Prestamo.cs
+++ src/biblioteca/Prestamo.cs
@@ -1,17 +1,25 @@
+// Clase que representa un préstamo de un libro a un usuario
 class Prestamo
 {
+    // Libro que se presta
     private Libro libroPrestado { get; set; }
+
+    // Usuario que realiza el préstamo
     private Usuario usuario { get; set; }
 
+    // Constructor: recibe el libro y el usuario
     public Prestamo(Libro libroPrestado, Usuario usuario)
     {
         this.libroPrestado = libroPrestado;
         this.usuario = usuario;
     }
 
+    // Método para mostrar la información del préstamo
     public void MostrarPrestamo()
     {
         Console.WriteLine("Préstamo realizado:");
-        Console.WriteLine(this.usuario.Nombre + " tomó prestado el libro.");
+
+        // Se muestra el nombre del usuario que tomó el libro
+        Console.WriteLine(this.usuario.Nombre + " tomó prestado el libro");
     }
 }
